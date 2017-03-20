21230c925549bcd68e00083d115393333cb5290e
kenpeter <figo2476@hotmail.com>
2017-03-20 12:17:15 +1100

    update

--- clip_to_music.js
+++ clip_to_music.js
@@ -2,13 +2,22 @@
 const glob = require('glob');
 // wait for
 const Promise = require('bluebird');
+// fs
 const fs = require('fs');
+// mp3
+const ffmpeg = require('fluent-ffmpeg');
 
-// file path
+// video source file path
 const videoPath = '/home/kenpeter/Videos/4K\ Video\ Downloader';
 
+// audio source file path
+const audioPath = __dirname + "/audio";
+
+// child process, exec
+const exec = require('child_process').exec;
+
 // now rename promise
-let renamePromise = new Promise((resolve, reject) => {
+var renamePromise = new Promise((resolve, reject) => {
   // search all mp4
   glob(videoPath + "/**/*.mp4", function (er, files) {
     // map each file
@@ -31,7 +40,129 @@ let renamePromise = new Promise((resolve, reject) => {
   }); // end glob
 }); // end promise
 
-renamePromise.then(() => {
-  console.log('--- all done ---');
-  process.exit(0);
+// music promise
+var musicPromise = new Promise((resolve, reject) => {
+  glob(videoPath + "/**/*.mp4", function (er, files) {
+    Promise.each(files, (singleClipFile) => {
+      return new Promise((resolve1, reject1) => {
+        // split
+        let arr = singleClipFile.split("/");
+
+        // e.g. xxxx.mp4
+        let clipFile = arr[arr.length - 1];
+
+        // e.g. xxxx no mp4
+        let fileName = clipFile.replace(/\.[^/.]+$/, "");
+
+        // music file name
+        let musicFile = fileName + '.mp3';
+
+        // set source
+        let proc = new ffmpeg({source: singleClipFile});
+
+        // set ffmpeg path
+        proc.setFfmpegPath('/usr/bin/ffmpeg');
+
+        // save mp3
+        proc.output("./audio/" + musicFile);
+
+        // proc on error
+        proc.on('error', (err) => {
+          console.log(err);
+        });
+
+        // done mp3 conversion
+        proc.on('end', (x) => {
+          console.log("single mp3 done!");
+          console.log(x);
+          // it is resolve1..............
+          resolve1();
+        });
+
+        // Run !!!!!!!!!!!!!
+        proc.run();
+
+      });
+    })
+    .then(() => {
+      console.log('--------- all mp3 conversion done --------');
+      resolve();
+    });
+
+  }); // end glob
 });
+
+// adb kill
+var adbKillPromise = new Promise((resolve, reject) => {
+  exec("adb kill-server", (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(stdout);
+    console.log('---adb kill---');
+    resolve();
+  });
+});
+
+// adb start
+var adbStartPromise = new Promise((resolve, reject) => {
+  exec("adb start-server", (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    console.log(stdout);
+    console.log('---adb start---');
+    resolve();
+  });
+});
+
+// adb push promise
+var adbPushPromise = new Promise((resolve, reject) => {
+  //
+  glob(audioPath + "/**/*.mp3", (er, files) => {
+    Promise.each(files, (singleMusicFile) => {
+      return new Promise((resolve1, reject1) => {
+        let cmd = "adb push" + " " + singleFile + " " + "/sdcard/Music";
+        exec(cmd, (err, stdout, stderr) => {
+          console.log(cmd);
+          resolve1();
+        });
+      });
+    })
+    .then(() => {
+      console.log('---- done push all music ---');
+      resolve();
+    });
+
+  });
+
+});
+
+
+// define your func
+function puts(error, stdout, stderr) {
+  console.log(stdout)
+}
+
+// Run !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+renamePromise
+  .then(() => {
+    return musicPromise;
+  })
+  .then(() => {
+    return adbKillPromise;
+  })
+  .then(() => {
+    return adbStartPromise;
+  })
+  .then(() => {
+    return adbPushPromise;
+  })
+  .then(() => {
+    console.log('---- all done----');
+    process.exit(0);
+  });
